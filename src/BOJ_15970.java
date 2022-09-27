cc0866701fa01622c5212a6fafa96c0c2a1cded3
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-09-27 16:42:19 +0900

    🔄 BOJ_15970

---
+++ src/BOJ_15970.java
@@ -0,0 +1,19 @@
+public class BOJ_15970 {
+
+    static MyReader sc = new MyReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() {
+
+    }
+
+    static void pro() {
+
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+}
