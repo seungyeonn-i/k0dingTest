056e00581e0873281c9eac20aa4698f56134b94b
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-10-10 17:18:41 +0900

    ☑️ BOJ_7795 이분 탐색

--- src/BOJ_7795.java
+++ src/BOJ_7795.java
@@ -1,65 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_7795 {
 
     /**
-   */
+     * Binary Search ..
+     * 정답의 최대치 4억 -> Integer .. 시간제한 1초
+     * 1. 정렬 : O(M log M)
+     * 2. 이분 탐색 : A의 원소마다 이분 탐색 -> O(N log M)
+
+     */
 
     static MyScanner sc = new MyScanner();
     static StringBuilder sb = new StringBuilder();
 
-    static int N,T,M;
-    static ArrayList<Integer> A,B;
+    static int N, M;
+    static int[] A, B;
 
     static void input() {
+        N = sc.nextInt();
+        M = sc.nextInt();
+        A = new int[N + 1];
+        B = new int[M + 1];
+        for (int i = 1; i <= N; i++) {
+            A[i] = sc.nextInt();
+        }
+        for (int i = 1; i <= M; i++) {
+            B[i] = sc.nextInt();
+        }
+    }
 
+    static int lower_bound(int[] A, int L, int R, int X) {
+        // A[L...R] 에서 X 미만의 수 중 제일 오른쪽 인덱스를 return 하는 함수
+        // 그런 게 없다면 L - 1 을 return 한다
 
-            N = sc.nextInt();
-            M = sc.nextInt();
-            A = new ArrayList<>();
-            B = new ArrayList<>();
-
-            for(int j=0;j<N;j++){
-                A.add(sc.nextInt());
+        int res = L - 1;  // 만약 A[L...R] 중 X 이하의 수가 없다면 L - 1 을 return 한다.
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (A[mid] < X) {
+                res = mid;
+                L = mid + 1;
+            } else {
+                R = mid - 1;
             }
-            for(int k=0;k<M;k++){
-                B.add(sc.nextInt());
-            }
-
+        }
+        return res;
     }
 
     static void pro() {
+        // B 배열에 대해 이분탐색을 할 예정이니까, 정렬을 해주자!
+        Arrays.sort(B, 1, M + 1);
 
-        int a=0;
-
-        Collections.sort(A);
-        Collections.sort(B);
-
-        for (Integer integer : A) {
-            for (Integer integer1 : B) {
-                if (integer > integer1) {
-                    a++;
-                }
-            }
+        int ans = 0;
+        for (int i = 1; i <= N; i++) {
+            // A[i] 를 선택했을 때, B 에서는 A[i]보다 작은 게 몇 개나 있는 지 count하기
+            ans += lower_bound(B, 1, M, A[i]);
         }
-        sb.append(a+"\n");
-
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
-
-        T = sc.nextInt();
-        for(int i=0;i<T;i++){
+        int TT;
+        TT = sc.nextInt();
+        for (int tt = 1; tt <= TT; tt++) {
             input();
             pro();
         }
-        System.out.println(sb);
     }
 }
 
