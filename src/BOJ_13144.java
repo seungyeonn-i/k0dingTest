b743f2b9dfb62856e47541292eca18cc194172ba
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-10-16 14:30:28 +0900

    ☑️ BOJ_13144:두 포인터

--- src/BOJ_13144.java
+++ src/BOJ_13144.java
@@ -1,18 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ_2470_1 {
-    /**
-     * 서로 다른 두 용액을 더해서 합이 최대한 0에 가깝게
-     */
-    static int N;
-    static int[] A,cnt;
+public class BOJ_13144 {
 
-//    scanner
-    static MyScanner2470 sc = new MyScanner2470();
+    static int N;
+    static int[] A;
+    static int[] cnt = new int[10000 + 1];
+
+    static MyScanner13144 sc = new MyScanner13144();
     static StringBuilder sb = new StringBuilder();
 
     static void input() {
@@ -21,60 +18,52 @@ public class BOJ_2470_1 {
         for (int i = 1; i <= N; i++) {
             A[i] = sc.nextInt();
         }
-
+        cnt = new int[100000 + 1];
     }
 
     static void pro() {
-
-        Arrays.sort(A, 1, N + 1);
-
-        int best_sum = Integer.MAX_VALUE;
-        int v1 = 0, v2 = 0, L = 1, R = N;
-
-        while (L < R) {
-            int sum = A[L] + A[R];
-            if (Math.abs(sum) < best_sum) {
-                best_sum = Math.abs(sum);
-                v1 = A[L];
-                v2 = A[R];
+        long ans = 0;
+        for (int L = 1, R = 0; L <= N; L++) {
+            // TODO : R을 옮길 수 있을 만큼 옮긴다
+            while (R + 1 <= N && cnt[A[R + 1]] == 0) {
+                R++;
+                cnt[A[R]]++;
             }
-            if (sum > 0) {
-                R--;
-            } else L++;
-
-        }sb.append(v1).append(' ').append(v2);
-
+            // TODO : 정답을 갱신한다
+            ans += R - L + 1;
+            // TODO : L을 옮겨주면서 A[L]의 개수를 감소시킨다.
+            cnt[A[L]]--;
+        }
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
         input();
         pro();
-        System.out.println(sb);
+
     }
 }
-class MyScanner2470{
+class MyScanner13144{
     BufferedReader br;
     StringTokenizer st;
 
-    public MyScanner2470(){
+    public MyScanner13144() {
         br = new BufferedReader(new InputStreamReader(System.in));
     }
 
-    public String next() {
-
-        while(st == null || !st.hasMoreElements()){
+    String next() {
+        String str="";
+        while (st == null || !st.hasMoreElements()) {
             try {
                 st = new StringTokenizer(br.readLine());
             } catch (IOException e) {
                 e.printStackTrace();
-
             }
         }
         return st.nextToken();
     }
 
-    public int nextInt() {
+    int nextInt() {
         return Integer.parseInt(next());
     }
-
 }
