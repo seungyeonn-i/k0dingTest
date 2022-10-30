05c303a5a738538f8416ce5c335bbddfcbb41d57
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-10-30 16:08:00 +0900

    ☑️ BOJ_11726:DP 점화식

---
+++ src/BOJ_11726.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11726 {
+    static int N;
+    static int [] dy;
+
+    static MyReader sc = new MyReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() {
+        N = sc.nextInt();
+    }
+    static void pro() {
+        dy = new int[N + 1];
+
+        dy[1] = 1;
+        dy[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dy[i] = (dy[i - 1] + dy[i - 2]) % 10007;
+        }
+        System.out.println(dy[N]);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class MyReader{
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+
+    }
+
+
+}
