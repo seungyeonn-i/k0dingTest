3978a6ff290c63372f6af42f80ef318bf7101cc7
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-11-03 16:26:54 +0900

    ☑️ BOJ_20165:시뮬레이션

--- src/BOJ_20165.java
+++ src/BOJ_20165.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 
 public class BOJ_20165 {
 
-    static int N,M,R;
+    // 시뮬레이션
+
+    static int N,M,R,ans;
     static int[][] ground;
     static String[] round;
     static int[][] status;
-    static int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
+//    static int[][] dir = {{1,0},{-1,0},{0,-1},{0,1}};
 
     static Scanner sc = new Scanner(System.in);
 
@@ -18,7 +20,6 @@ public class BOJ_20165 {
 
         ground = new int[N + 1][M + 1];
         status = new int[N + 1][M + 1];
-        round = new String[2*R + 1];
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++) {
@@ -28,31 +29,29 @@ public class BOJ_20165 {
             }
         }
 
-        for (int k = 1; k <= R*2; k++) {
-            round[k] = sc.nextLine();
-        }
     }
 
-    static void attack(int x , int y, int k, char way) {
+    static void attack(int x , int y, int way) {
+        // cnt ; 연쇄적으로 넘어질 도미노 개수
+        // max(cnt-1,h-1)
+        int dir[][] = {{0,1},{0,-1},{1,0},{-1,0}}; //???????
 
-        if(way=='E') way = 0;
+        if(way == 'E') way = 0;
         else if(way == 'W') way = 1;
         else if(way == 'S') way = 2;
         else if(way == 'N') way = 3;
 
-        for(int i=0;i<k-1;i++){
-            if(status[x+dir[way][0]][y+dir[way][1]] == -1 ) return ;
-            if(x+dir[way][0] > N || y+dir[way][1] > M) return ;
+        int cnt = status[x][y];
+        while (x > 0 && x <= N && y > 0 && y <= M && cnt > 0) {
+            if(status[x][y] != -1 ) ans++;
+            cnt = Math.max(cnt - 1, status[x][y] - 1);
+            status[x][y] = -1;
 
-            status[x+dir[way][0]][y+dir[way][1]] = -1;
             x += dir[way][0];
             y += dir[way][1];
         }
     }
 
-    static void defense(int x, int y) {
-        status[x][y] = ground[x][y];
-    }
 
     static void print() {
         for (int i = 1; i <= N; i++) {
@@ -67,18 +66,25 @@ public class BOJ_20165 {
     }
 
     static void pro() {
-        attack(3,1, status[3][1],'E');
-        defense(3, 5);
-        attack(5, 3, status[5][3], 'N');
-        defense(3, 3);
-        attack(5, 2, status[5][2], 'N');
-        defense(3, 1);
+        for (int rep = 1; rep <= R; rep++) {
+            int x,y;
+            String dir;
+
+            x = sc.nextInt();
+            y = sc.nextInt();
+            dir = sc.next();
+            attack(x, y, dir.charAt(0));
+
+            x = sc.nextInt();
+            y = sc.nextInt();
+            status[x][y] = ground[x][y];
+
+        }
+        print();
     }
 
     public static void main(String[] args) {
         input();
         pro();
-        System.out.println();
-        print();
     }
 }
