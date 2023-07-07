2a9b5c4f89a42694926bfbab762573f6870252d5
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-07-08 01:23:58 +0900

    This is codingTest : 2day

---
+++ python/book/DP/8-8.py
@@ -0,0 +1,20 @@
+n,m=map(int,input().split(" "))
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+
+dp = [10001]*(m+1)
+
+# dp[i] = min(dp[i],dp[i-k+1) , k는 화폐 단위 
+
+dp[0] = 0
+for i in range(n):
+    for j in range(arr[i],m+1):
+        if dp[j-arr[i]] != 1001:
+            dp[j] = min(dp[j], dp[j-arr[i]]+1)
+
+if dp[m] == 10001:
+    print(-1)
+else:
+    print(dp[m])
+
